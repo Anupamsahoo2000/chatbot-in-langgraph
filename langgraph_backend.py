fdf474b62730bd119d6cd83a9db49ec60f23f5b7
Anupam2000 <anupamsahoo2000@gmail.com>
2026-09-17 11:58:34 +0530

    backend done

---
+++ langgraph_backend.py
@@ -0,0 +1,33 @@
+from typing import Annotated, TypedDict
+
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph import END, START, StateGraph
+from langgraph.graph.message import add_messages
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+def chat_node(state: ChatState):
+    messages = state["messages"]
+    response = llm.invoke(messages)
+
+    return {"messages": [response]}
+
+
+chaeckpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=chaeckpointer)
